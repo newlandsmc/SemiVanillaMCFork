e4e8d4674634cb2fb57544e2d84d4a1f41c2d188
destro174 <40720638+destro174@users.noreply.github.com>
2022-03-15 10:31:02 +0100

    rebranch to SemiVanilla

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ subprojects {
 }
 
 paperweight {
-    serverProject.set(project(":forktest-server"))
+    serverProject.set(project(":SemiVanilla-server"))
 
     remapRepo.set("https://maven.fabricmc.net/")
     decompileRepo.set("https://files.minecraftforge.net/maven/")
@@ -58,10 +58,10 @@ paperweight {
     usePaperUpstream(providers.gradleProperty("paperRef")) {
         withPaperPatcher {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("forktest-api"))
+            apiOutputDir.set(layout.projectDirectory.dir("SemiVanilla-api"))
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("forktest-server"))
+            serverOutputDir.set(layout.projectDirectory.dir("SemiVanilla-server"))
         }
     }
 }
@@ -69,45 +69,45 @@ paperweight {
 //
 // Everything below here is optional if you don't care about publishing API or dev bundles to your repository
 //
+// No need for this yet
+//tasks.generateDevelopmentBundle {
+//    apiCoordinates.set("com.semivanilla.paperfork:SemiVanilla-api")
+//    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
+//    libraryRepositories.set(
+//        listOf(
+//            "https://repo.maven.apache.org/maven2/",
+//            "https://libraries.minecraft.net/",
+//            "https://papermc.io/repo/repository/maven-public/",
+//            "https://maven.quiltmc.org/repository/release/",
+//            // "https://my.repo/", // This should be a repo hosting your API (in this example, 'com.example.paperfork:forktest-api')
+//        )
+//    )
+//}
 
-tasks.generateDevelopmentBundle {
-    apiCoordinates.set("com.example.paperfork:forktest-api")
-    mojangApiCoordinates.set("io.papermc.paper:paper-mojangapi")
-    libraryRepositories.set(
-        listOf(
-            "https://repo.maven.apache.org/maven2/",
-            "https://libraries.minecraft.net/",
-            "https://papermc.io/repo/repository/maven-public/",
-            "https://maven.quiltmc.org/repository/release/",
-            // "https://my.repo/", // This should be a repo hosting your API (in this example, 'com.example.paperfork:forktest-api')
-        )
-    )
-}
+//allprojects {
+//    // Publishing API:
+//    // ./gradlew :ForkTest-API:publish[ToMavenLocal]
+//    publishing {
+//        repositories {
+//            maven {
+//                name = "myRepoSnapshots"
+//                url = uri("https://my.repo/")
+//                // See Gradle docs for how to provide credentials to PasswordCredentials
+//                // https://docs.gradle.org/current/samples/sample_publishing_credentials.html
+//                credentials(PasswordCredentials::class)
+//            }
+//        }
+//    }
+//}
 
-allprojects {
-    // Publishing API:
-    // ./gradlew :ForkTest-API:publish[ToMavenLocal]
-    publishing {
-        repositories {
-            maven {
-                name = "myRepoSnapshots"
-                url = uri("https://my.repo/")
-                // See Gradle docs for how to provide credentials to PasswordCredentials
-                // https://docs.gradle.org/current/samples/sample_publishing_credentials.html
-                credentials(PasswordCredentials::class)
-            }
-        }
-    }
-}
-
-publishing {
-    // Publishing dev bundle:
-    // ./gradlew publishDevBundlePublicationTo(MavenLocal|MyRepoSnapshotsRepository) -PpublishDevBundle
-    if (project.hasProperty("publishDevBundle")) {
-        publications.create<MavenPublication>("devBundle") {
-            artifact(tasks.generateDevelopmentBundle) {
-                artifactId = "dev-bundle"
-            }
-        }
-    }
-}
+//publishing {
+//    // Publishing dev bundle:
+//    // ./gradlew publishDevBundlePublicationTo(MavenLocal|MyRepoSnapshotsRepository) -PpublishDevBundle
+//    if (project.hasProperty("publishDevBundle")) {
+//        publications.create<MavenPublication>("devBundle") {
+//            artifact(tasks.generateDevelopmentBundle) {
+//                artifactId = "dev-bundle"
+//            }
+//        }
+//    }
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,6 @@ pluginManagement {
     }
 }
 
-rootProject.name = "forktest"
+rootProject.name = "SemiVanillaMC"
 
-include("forktest-api", "forktest-server")
+include("SemiVanilla-api", "SemiVanilla-server")
